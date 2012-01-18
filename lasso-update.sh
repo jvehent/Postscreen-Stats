08c639736e2bf2b38024d8a55ee61655de647139
Julien Vehent <julien@linuxwall.info>
2012-01-17 19:31:57 -0500

    added useful shell scripts

---
+++ lasso-update.sh
@@ -0,0 +1,23 @@
+#! /bin/sh
+URL=http://www.spamhaus.org/drop/drop.lasso
+DROPLASSO=$(mktemp)
+ACLFILE=$(mktemp)
+POSTSCREEN_ACCESS_FILE=/etc/postfix/postscreen_access.cidr
+DATE=$(date +%Y%m%d)
+
+wget --quiet $URL -O $DROPLASSO
+
+if [ -e $DROPLASSO ]
+then
+  # decoupe le fichier source sur le point virgule
+  # et prepare le fichier de destination
+  for network in $(cat $DROPLASSO | awk '{print $1}' |\
+    grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$")
+  do
+    echo -e "$network\treject" >> $ACLFILE
+  done
+fi
+
+cp $POSTSCREEN_ACCESS_FILE{,.bkp$DATE}
+cp $ACLFILE $POSTSCREEN_ACCESS_FILE
+postfix reload
