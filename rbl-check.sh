738b16fbae2db1f7a0dc6da49cd440e1fda1a1ae
Matt Christian <matt.christian@gmail.com>
2016-09-16 16:08:57 -0500

    Rework the entire DNSbl list: remove dead/invalid/redudant lists, add several new/updated lists. Split variable into multiple lines. Also try to get any TXT DNS records from the DNSbls and show them. Properly display results from lists that return multiple A records. Add IP 127.0.0.2 commented out for testing DNSbls. Add multiple comments explaining the commands.

--- rbl-check.sh
+++ rbl-check.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 DEBUG="$1"
-# replace the SRV list with your own machines
-SRV="mail.example.com mail.example.net"
-RBL="bl.spamcop.net cbl.abuseat.org b.barracudacentral.org dnsbl.invaluement.com ddnsbl.internetdefensesystems.com dnsbl.sorbs.net http.dnsbl.sorbs.net dul.dnsbl.sorbs.net misc.dnsbl.sorbs.net smtp.dnsbl.sorbs.net socks.dnsbl.sorbs.net spam.dnsbl.sorbs.net web.dnsbl.sorbs.net zombie.dnsbl.sorbs.net dnsbl-1.uceprotect.net dnsbl-2.uceprotect.net dnsbl-3.uceprotect.net pbl.spamhaus.org sbl.spamhaus.org xbl.spamhaus.org zen.spamhaus.org bl.spamcannibal.org psbl.surriel.com ubl.unsubscore.com dnsbl.njabl.org combined.njabl.org rbl.spamlab.com dnsbl.ahbl.org ircbl.ahbl.org dyna.spamrats.com noptr.spamrats.com spam.spamrats.com cbl.anti-spam.org.cn cdl.anti-spam.org.cn dnsbl.inps.de drone.abuse.ch httpbl.abuse.ch dul.ru korea.services.net short.rbl.jp virus.rbl.jp spamrbl.imp.ch wormrbl.imp.ch virbl.bit.nl rbl.suresupport.com dsn.rfc-ignorant.org ips.backscatterer.org spamguard.leadmon.net opm.tornevall.org netblock.pedantic.org black.uribl.com grey.uribl.com multi.surbl.org ix.dnsbl.manitu.net tor.dan.me.uk rbl.efnetrbl.org relays.mail-abuse.org blackholes.mail-abuse.org rbl-plus.mail-abuse.org dnsbl.dronebl.org access.redhawk.org db.wpbl.info rbl.interserver.net query.senderbase.org bogons.cymru.com"
+# Replace the SRV list with your own machines' full Internet DNS names
+SRV="localhost"
+# Build up the RBL list using the bash += append operator
+RBL="bl.spamcop.net b.barracudacentral.org zen.spamhaus.org free.v4bl.org "
+RBL+="dnsbl.sorbs.net spam.dnsbl.sorbs.net truncate.gbudb.net all.s5h.net "
+RBL+="dnsbl-1.uceprotect.net dnsbl-2.uceprotect.net dnsbl-3.uceprotect.net "
+RBL+="bl.spamcannibal.org psbl.surriel.com ubl.unsubscore.com db.wpbl.info "
+RBL+="dyna.spamrats.com noptr.spamrats.com spam.spamrats.com "
+RBL+="dnsbl.inps.de drone.abuse.ch httpbl.abuse.ch korea.services.net "
+RBL+="short.rbl.jp virus.rbl.jp spamrbl.imp.ch wormrbl.imp.ch virbl.bit.nl "
+RBL+="ips.backscatterer.org spamguard.leadmon.net dnsbl.tornevall.org "
+RBL+="ix.dnsbl.manitu.net tor.dan.me.uk bad.psky.me rbl.efnetrbl.org "
+RBL+="dnsbl.dronebl.org access.redhawk.org hostkarma.junkemailfilter.com "
+RBL+="rbl.interserver.net query.senderbase.org bogons.cymru.com "
 for server in $SRV
 do
+    # Resolve the DNS name into an Internet IP address
     ip=$(dig +short $server)
+    # Testing IP address = 127.0.0.2
+    #ip="127.0.0.2"
+    # Reverse the IP address octets for DNSbl check format
     r_ip=$(echo $ip|awk -F"." '{for(i=NF;i>0;i--) printf i!=1?$i".":"%s",$i}')
     for rbl in $RBL
     do
@@ -16,7 +31,16 @@ do
         result=$(dig +short $r_ip.$rbl)
         if [ ! -z "$result" ]
         then
-            echo "$server ($ip) is in $rbl with code $result"
+            # Some DNSbls return multiple results, change newlines to spaces
+            echo -n "$server ($ip) is in $rbl with result ${result//$'\n'/ }"
+            # Also try to get any TXT DNS records
+            text=$(dig +short $r_ip.$rbl TXT)
+            if [ ! -z "$text" ]
+            then
+                echo " and text ${text//$'\n'/ }"
+            else
+                echo ""
+            fi
         fi
         if [[ ! -z "$DEBUG" && -z "$result" ]]
         then
