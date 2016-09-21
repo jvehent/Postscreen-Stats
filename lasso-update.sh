0db6dbf5db969958582c6a512c2745a77f1116c5
Matt Christian <matt.christian@gmail.com>
2016-09-21 13:28:00 -0500

    Try to use wget first, otherwise curl. Fix SC2013 and SC2086 issues from ShellCheck.

--- lasso-update.sh
+++ lasso-update.sh
@@ -6,19 +6,24 @@ ACLFILE=$(mktemp)
 POSTSCREEN_ACCESS_FILE=/etc/postfix/postscreen_access.cidr
 DATE=$(date +%Y%m%d)
 
-wget --quiet $URL -O $DROPLASSO
+# Try to use wget first, otherwise curl
+if ! wget --quiet -O "$DROPLASSO" $URL ;
+then
+  curl --silent -o "$DROPLASSO" $URL
+fi
 
-if [ -e $DROPLASSO ]
+if [ -e "$DROPLASSO" ] ;
 then
   # cut the source file on the semicolon
   # and prepare the destination file
-  for network in $(cat $DROPLASSO | awk '{print $1}' |\
-    grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$")
+  awk '{print $1}' "$DROPLASSO" |\
+    grep -E "^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$" |\
+    while IFS= read -r network
   do
-    echo -e "$network\treject" >> $ACLFILE
+    echo -e "$network\treject" >> "$ACLFILE"
   done
 fi
 
-cp $POSTSCREEN_ACCESS_FILE{,.bkp$DATE}
-cp $ACLFILE $POSTSCREEN_ACCESS_FILE
+cp $POSTSCREEN_ACCESS_FILE{,.bkp"$DATE"}
+cp "$ACLFILE" $POSTSCREEN_ACCESS_FILE
 postfix reload
